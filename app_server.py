3804b94bf1d6124e392717c33b006e21d68d509a
alanliu <alan@qoo-app.com>
2019-03-18 22:50:52 +0800

    feat [app_server] Add URL endpoint for getting tweets by hashtag (#1)
    Then endpoint returns a list of tweets if success and raise a 404-error
    if there is not tweet for this hashtag.
    (#1)

--- app_server.py
+++ app_server.py
@@ -18,3 +18,15 @@ def debug_http_error(e):
 def root_view():
     return 'hello'
 
+
+@app.route('/hashtags/<string:hashtag>')
+def get_by_hash_tag(hashtag):
+    limit = request.args.get('limit')
+    client = twitter_client.Client(settings.API_KEY, settings.API_SECRET_KEY)
+    client.auth()
+
+    tweets = client.get_tweets_by_hashtag(hashtag, limit=limit)
+    if not tweets:
+        abort(404)
+    return jsonify(tweets)
+
